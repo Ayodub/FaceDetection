72d6978c7416aa2a34eb5f009edcb4e3d70493a1
Ayodub <66164546+Ayodub@users.noreply.github.com>
2024-07-15 13:29:03 +1000

    Add files via upload

---
+++ FacialDetection/Program.fs
@@ -0,0 +1,35 @@
+﻿
+open System
+open Accord.Vision.Detection
+open Accord.Vision.Detection.Cascades
+open Accord.Imaging.Filters
+open System.Drawing
+
+
+
+
+[<EntryPoint>]
+let main argv =
+    let detector = new HaarObjectDetector (new FaceHaarCascade(),30)
+    detector.SearchMode <- ObjectDetectorSearchMode.NoOverlap
+    detector.ScalingMode <- ObjectDetectorScalingMode.SmallerToGreater
+    detector.ScalingFactor <- 1.5f
+    detector.UseParrallelProcessing <- true
+    detector.Suppression <- 2
+
+    let img = new Bitmap("some-guy.jpg")
+    let objs = detector.ProcessFrame(img)
+
+
+    if (objs.Length) > 0 then
+        let marker = new RectanglesMarker(objs, Color.Fuschia);
+        let newImg = marker.Apply(img)
+        newImg.Save(@".\some-guy-with-box-around-face.jpg")
+        printfn "Face detected from F#!"
+        0
+
+    else 
+        printfn "Face Not Detected from F#!"
+        0
+  
+     
